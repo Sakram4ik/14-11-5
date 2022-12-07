55746ff3e6f31ad79bdcd4e128aadf07edfb351b
Illia Cherednik <91588797+Sakram4ik@users.noreply.github.com>
2022-12-07 09:12:14 +0000

    add coments

--- .ccls-cache/@home@runner@14115/main.cpp
+++ .ccls-cache/@home@runner@14115/main.cpp
@@ -8,12 +8,12 @@ int main(){
   int column=0;//j 
   int line=0;//i
 	srand(time(NULL));//отримує у вигляді параметра поточний системний час, який при кожному запуску програми буде різним. 
-	for (int i = 0; i < 3; ++i) { // для рядків 
-		for (int j = 0; j < 4; ++j) {// для стовпчиків
+	for (int i = 0; i < 3; i++) { // для рядків 
+		for (int j = 0; j < 4; j++) {// для стовпчиків
 			arr[i][j] =rand() %10;
 		}
 	}
-	for (int i = 0; i < 3; ++i) {
+	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 4; j++) {
 			if(value<arr[i][j]){
         value=arr[i][j];
@@ -23,8 +23,8 @@ int main(){
 		}
 	}
   cout << "for "<<line <<" line and "<<column <<" column biggest number = " << value << endl;
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 4; ++j) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
 			cout << arr[i][j] << ' ';
 		}//виведення двовимірного масиву
 		cout << endl;
